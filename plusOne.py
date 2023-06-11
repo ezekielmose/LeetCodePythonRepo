e36a05b92b91ac166ec6ecc9cae71d5add1cf14e
ezekiel <ezekielmose97@gmail.com>
2023-06-11 10:05:05 +0300

    Plus One , made some changes

--- plusOne.py
+++ plusOne.py
@@ -31,13 +31,13 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits)-1,-1,-1):
-            if digits[i]<9:
-                digits[i]+=1
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            digits[i] += 1
+            digits[i] %= 10
+            if digits[i] != 0:
                 return digits
-            digits[i]=0
-
-        return [1]+[0]*len(digits)
+        return [1] + digits
 if __name__=='main':
     r=Solution
     print(r.plusOne(1,1,2,3,1))
