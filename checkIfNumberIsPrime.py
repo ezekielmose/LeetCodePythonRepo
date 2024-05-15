f6557febd61edf4c14eb573b4fc00416ae344917
ezekiel <ezekielmose97@gmail.com>
2024-05-15 15:07:21 +0300

    Counting the number of frequency

---
+++ checkIfNumberIsPrime.py
@@ -0,0 +1,13 @@
+def is_prime (n):
+    if n<2:
+        return False
+    for i in range (2, int(n**0.5)+1):
+        if n %2==0:
+            return False
+    return  True
+
+nums=20
+if is_prime(nums):
+    print(f"{nums} is a prime number")
+else:
+    print(f"{nums} is not a prime number")
